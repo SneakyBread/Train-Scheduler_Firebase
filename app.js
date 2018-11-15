34fd71dad7260eef0ae37489d5bf6a7bf01c3030
Nathan <nathanlserrano@gmail.com>
2018-11-14 22:02:25 -0800

    Finished initial design template

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCnuP8d0Hlz5I6AT7KyokwuPfoNo1vxdLY",
+    authDomain: "trainscheduleryay.firebaseapp.com",
+    databaseURL: "https://trainscheduleryay.firebaseio.com",
+    projectId: "trainscheduleryay",
+    storageBucket: "",
+    messagingSenderId: "433369364964"
+  };
+  firebase.initializeApp(config);
+
+
+});
